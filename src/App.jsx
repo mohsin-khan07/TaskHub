2dcacd239933a2f6c811ee52b15d15f1bbcb6926
Mohsin Khan <mkhan6720@gmail.com>
2024-03-31 23:40:46 +0530

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>app</div>;
+  return (
+    <div className="m-2 flex flex-col justify-center bg-blue-50 p-2 font-bold text-black">
+      app
+    </div>
+  );
 }
 
 export default App;
